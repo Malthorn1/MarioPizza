ec1aa59263623c7d2238c842373bf3416cea2eb6
Bringordie <48799015+Bringordie@users.noreply.github.com>
2019-04-05 09:56:25 +0200

    Update JavaApplication25.java

--- JavaApplication25/src/javaapplication25/JavaApplication25.java
+++ JavaApplication25/src/javaapplication25/JavaApplication25.java
@@ -5,7 +5,7 @@ public class JavaApplication25 {
 
 
     public static void main(String[] args) {
-        System.out.println("This is a test. Hello World.");
+        System.out.println("This is a test. Hello World. How are you.");
 
     }
 
