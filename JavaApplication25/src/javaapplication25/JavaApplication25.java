b4fe85806f59427ed67089936719297a2450c090
Frederik <frbraagaard@hotmail.com>
2019-04-05 09:50:55 +0200

    Hello dis is a test

---
+++ JavaApplication25/src/javaapplication25/JavaApplication25.java
@@ -0,0 +1,12 @@
+package javaapplication25;
+
+
+public class JavaApplication25 {
+
+
+    public static void main(String[] args) {
+        System.out.println("This is a test. Hello World.");
+
+    }
+
+}
