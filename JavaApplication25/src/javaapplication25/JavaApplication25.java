29a25714858daf538868ac3b41b3e6636362c053
Frederik <frbraagaard@hotmail.com>
2019-04-05 10:02:02 +0200

    Hello dis is a test

--- JavaApplication25/src/javaapplication25/JavaApplication25.java
+++ JavaApplication25/src/javaapplication25/JavaApplication25.java
@@ -5,7 +5,7 @@ public class JavaApplication25 {
 
 
     public static void main(String[] args) {
-        System.out.println("This is a test. Hello World. How are you.");
+        System.out.println("This is a test. Hello World. How are you. I am fine");
 
     }
 
