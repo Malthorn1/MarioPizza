0d5b842c44b24a72985bf20cb0de6e16a95ebdb7
cprejler <prejler93@gmail.com>
2019-04-10 17:51:29 +0200

    Har fikset opretBestilling() Så man nu kan kalde flere bestillinger fra main, og de rent faktisk bliver skrevet ind i AKTIVEORDRER i SQL data basen. Du kan ikke auto incremente en SQL værdi i Java koden, men hvis man bare lader være med at skrive værdien i INSERT, sørger SQL selv for at auto incremente værdien.

--- MarioProject/src/datalayer/Database.java
+++ MarioProject/src/datalayer/Database.java
@@ -15,16 +15,16 @@ import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 public class Database implements DB {
-    static final long ONE_MINUTE_IN_MILLIS=60000;//millisecs
-    Calendar date = Calendar.getInstance(); 
-    long t=date.getTimeInMillis(); 
-    Date AddingTenMins=new Date(t + (10 *ONE_MINUTE_IN_MILLIS));
+//    static final long ONE_MINUTE_IN_MILLIS=60000;//millisecs
+//    Calendar date = Calendar.getInstance(); 
+//    long t=date.getTimeInMillis(); 
+//    Date AddingTenMins=new Date(t + (10 *ONE_MINUTE_IN_MILLIS));
 
     public Connection connector() {
         Connection connection = null;
         try {
             String user = "root";
-            String password = "frb150195";
+            String password = "rootprejler";
             String IP = "localhost";
             String PORT = "3306";
             String DATABASE = "mario";
@@ -80,60 +80,70 @@ public class Database implements DB {
         return null;
     }
 
-//    @Override
-//    public void opretBestilling(Pizza pizza) throws SQLException {
-//        //Skal indlæse alle værdier fra aktive ordrer og derefter skrive til aktiveordrer med det pizza objekt. 
-//        Database db = new Database();
-//        Connection connection = db.connector();
-//
-//        try {
-//            Statement st = connection.createStatement();
-//            ResultSet rs = st.executeQuery("select * from aktiveordrer");
-//            if (rs.next()) {
-//                String sql = "insert into aktiveordrer(ORDRENUMMER, FÆRDIG, DATOOPRETTET, DATOFÆRDIG, PIZZANUMMER)VALUES(?,?,?,?,?)";
-//                PreparedStatement statement = connection.prepareStatement(sql);
-//                statement.setInt(1, rs.getInt("ORDRENUMMER"));
-//                statement.setBoolean(2, rs.getBoolean("FÆRDIG"));
-//                statement.setTimestamp(3, rs.getTimestamp("DATOOPRETTET"));
-//                statement.setDate(4, rs.getDate("DATOFÆRDIG"));
-//                statement.setInt(5, pizza.getPizzaNummer());
-//                statement.execute();
-//                
-//            }
-//            
-//        } catch (SQLException e) {
-//        }
-//    }
-    
-    //GAMLE METODE IN CASE OF EMERGENCY
     @Override
     public void opretBestilling(Pizza pizza) throws SQLException {
+        //Skal indlæse alle værdier fra aktive ordrer og derefter skrive til aktiveordrer med det pizza objekt. 
+        //Man er nød til at hente SQL typerne fra datbasen før man kan skrive dem ind igen i Java
         Database db = new Database();
         Connection connection = db.connector();
 
         try {
-            pizza = getPizza(pizza.getPizzaNummer());
-            Statement stat = connection.createStatement();
-            int ORDRENUMMER = 1;
-            ResultSet rs = stat.executeQuery("SELECT TIMESTAMP(NOW()) as timestamp");
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery("select * from aktiveordrer");
             if (rs.next()) {
-                Timestamp ts = rs.getTimestamp("timestamp");
-                Date dnow = new Date(System.currentTimeMillis()+5*60*1000);
-                String sql = "INSERT INTO aktiveordrer(ORDRENUMMER, FÆRDIG, DATOOPRETTET, DATOFÆRDIG, PIZZANUMMER)VALUES(?,?,?,?,?)";
+                boolean færdig = rs.getBoolean("FÆRDIG");
+                System.out.println(færdig);
+                Timestamp timestamp = rs.getTimestamp("DATOOPRETTET");
+                System.out.println(timestamp);
+                java.sql.Date dt = rs.getDate("DATOFÆRDIG");
+                System.out.println(dt);
+                int pizzanummer = pizza.getPizzaNummer();
+                System.out.println(pizzanummer);
+                String sql = "INSERT INTO aktiveordrer(FÆRDIG, DATOOPRETTET, DATOFÆRDIG, PIZZANUMMER)VALUES(?,?,?,?)";
                 PreparedStatement statement = connection.prepareStatement(sql);
-                statement.setInt(1, ORDRENUMMER + 1);
-                statement.setBoolean(2, false);
-                statement.setTimestamp(3, ts);
-                statement.setDate(4, dnow);
-                statement.setInt(5, pizza.getPizzaNummer());
-                statement.execute();
-                               
+                //Vi springer første kolonne over "ORDRENUMMER" da den er sat til AUTO_INCREMENT i SQL, så SQL sørger selv for at incremente den hver gang der bliver
+                //Skrevet noget ind
+                statement.setBoolean(1, færdig);
+                statement.setTimestamp(2, timestamp);
+                statement.setDate(3, dt);
+                statement.setInt(4, pizzanummer);
+                statement.executeUpdate();
             }
 
         } catch (SQLException e) {
+            System.out.println(e);
         }
     }
 
+//    //GAMLE METODE IN CASE OF EMERGENCY
+//    @Override
+//    public void opretBestilling(Pizza pizza) throws SQLException {
+//        Database db = new Database();
+//        Connection connection = db.connector();
+//
+//        try {
+//            pizza = getPizza(pizza.getPizzaNummer());
+//            Statement stat = connection.createStatement();
+//            int ORDRENUMMER = 1;
+//            ResultSet rs = stat.executeQuery("SELECT TIMESTAMP(NOW()) as timestamp");
+//            if (rs.next()) {
+//                Timestamp ts = rs.getTimestamp("timestamp");
+//                Date dnow = new Date(System.currentTimeMillis()+5*60*1000);
+//                String sql = "INSERT INTO aktiveordrer(ORDRENUMMER, FÆRDIG, DATOOPRETTET, DATOFÆRDIG, PIZZANUMMER)VALUES(?,?,?,?,?)";
+//                PreparedStatement statement = connection.prepareStatement(sql);
+//                statement.setInt(1, ORDRENUMMER + 1);
+//                statement.setBoolean(2, false);
+//                statement.setTimestamp(3, ts);
+//                statement.setDate(4, dnow);
+//                statement.setInt(5, pizza.getPizzaNummer());
+//                statement.execute();
+//                statement.close();
+//                               
+//            }
+//
+//        } catch (SQLException e) {
+//        }
+//    }
     @Override
     public void fjernBestilling() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
